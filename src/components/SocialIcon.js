adf37a66a81e53f8b0f4aff666fc0867de9bf740
Kortney <kortney@iMac.local>
2022-06-03 16:19:39 -0300

    fixed warnings

--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function SocialIcon(props) {
     return (
         <div className="footer--icon">
-            <img src={props.iconURL} />
+            <img src={props.iconURL} alt="" />
         </div>
     );
 }
